c7370ab66a5048371a18f3ec2786457964d0f803
thedanielras <ras98.daniel@gmail.com>
2020-10-09 00:40:47 +0300

    feat: update AppDrawer custom drawer component

--- components/AppDrawer.js
+++ components/AppDrawer.js
@@ -1,35 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from 'react';
 import {
   View,
   Text,
   useWindowDimensions,
   Switch,
   StyleSheet,
-} from "react-native";
+} from 'react-native';
 import {
   DrawerContentScrollView,
   DrawerItemList,
-} from "@react-navigation/drawer";
+} from '@react-navigation/drawer';
+
+import { useTheme } from './ThemeManager';
 
 export default function AppDrawer(props) {
   const windowHeight = useWindowDimensions().height;
 
   const [isSwitchEnabled, setIsSwitchEnabled] = useState(false);
   const toggleSwitch = () => setIsSwitchEnabled((prevState) => !prevState);
+  const { theme, toggleTheme } = useTheme();
+
+  useEffect(() => {
+    if (isSwitchEnabled) toggleTheme('dark');
+    else toggleTheme('light');
+  }, [isSwitchEnabled]);
+
+  const styles = StyleSheet.create({
+    label: {
+      alignSelf: 'center',
+      fontSize: 16,
+      color: theme.onBackground,
+      fontWeight: 'bold',
+    },
+  });
 
   return (
-    <DrawerContentScrollView {...props}>
+    <DrawerContentScrollView
+      style={{ backgroundColor: theme.background, padding: 0 }}
+      contentContainerStyle={{ paddingTop: 0 }}
+      {...props}
+    >
       <View
         style={{
           height: Math.floor((windowHeight / 100) * 13),
-          backgroundColor: "#000",
+          backgroundColor: theme.secondary,
           flex: 10,
-          alignItems: "center",
-          justifyContent: "center",
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
-        <Text style={{ color: "#fff", fontSize: 21, fontWeight: "bold" }}>
-          Daniel's Demo App
+        <Text
+          style={{ color: theme.onSecondary, fontSize: 21, fontWeight: 'bold' }}
+        >
+          React Native Demo App
         </Text>
       </View>
       <View
@@ -44,37 +67,38 @@ export default function AppDrawer(props) {
       <View
         style={{
           flex: 10,
-          alignItems: "center",
-          justifyContent: "flex-start",
+          alignItems: 'center',
+          justifyContent: 'flex-start',
         }}
       >
         <View
-          style={{ width: "100%", borderTopColor: "#ccc", borderTopWidth: 1 }}
+          style={{ width: '100%', borderTopColor: '#ccc', borderTopWidth: 1 }}
         />
         <View
           style={{
-            flexDirection: "row",
-            alignItems: "center",
+            flexDirection: 'row',
+            alignItems: 'center',
             paddingTop: 10,
             paddingBottom: 10,
           }}
         >
           <Text style={{ ...styles.label, marginRight: 20 }}>
-            {"Use Dark Theme"}
+            {'Use Dark Theme'}
           </Text>
-          <Switch onValueChange={toggleSwitch} value={isSwitchEnabled} />
+          <Switch
+            trackColor={{
+              false: theme.primaryVariant,
+              true: theme.secondaryVariant,
+            }}
+            thumbColor={!isSwitchEnabled ? theme.primary : theme.secondary}
+            onValueChange={toggleSwitch}
+            value={isSwitchEnabled}
+          />
         </View>
         <View>
-          <Text>Version 0.1</Text>
+          <Text style={{ color: theme.onBackground }}>Version 0.1</Text>
         </View>
       </View>
     </DrawerContentScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  label: {
-    alignSelf: "center",
-    fontSize: 16,
-  },
-});
