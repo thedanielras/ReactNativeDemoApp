eb71c7a896031ec9f3890b0a2e385f4e16fc7536
thedanielras <ras98.daniel@gmail.com>
2020-10-09 00:41:20 +0300

    feat: add theme support to Header component

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,61 @@
-import React, { useState } from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
-import { getStatusBarHeight } from "react-native-status-bar-height";
-import Icon from "react-native-ionicons";
-import { useFonts } from "expo-font";
-import { AppLoading } from "expo";
-import { Switch } from "react-native-gesture-handler";
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, Button, StatusBar } from 'react-native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import Icon from 'react-native-ionicons';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo';
+import { Switch } from 'react-native-gesture-handler';
+
+import { useTheme } from './ThemeManager';
 
 export default function Header(props) {
   let [fontsLoaded] = useFonts({
-    Ionicons: require("../node_modules/react-native-ionicons/fonts/Ionicons.ttf"),
+    Ionicons: require('../node_modules/react-native-ionicons/fonts/Ionicons.ttf'),
   });
-  
+
+  const { theme } = useTheme();
+
+  let styles = StyleSheet.create({
+    container: {
+      backgroundColor: theme.isLight? theme.primary : theme.topSurface,
+      padding: 10,
+      flex: 1,
+      flexDirection: 'row',
+      alignItems: 'center',
+    },
+    title: {
+      fontSize: 21,
+      fontWeight: 'bold',
+      color: theme.onPrimary,
+    },
+  });
+
   if (!fontsLoaded) return <AppLoading />;
   return (
     <View style={styles.container}>
+      <StatusBar
+        backgroundColor={theme.isLight? theme.primaryVariant : "#000000"}
+        barStyle={'light-content'}
+      />
       <View style={{ flex: 1 }}>
-        <View style={{ alignItems: "center" }}>
-          <Icon name="menu" onPress={() => props.onMenuPress()}></Icon>
+        <View style={{ alignItems: 'center' }}>
+          <Icon
+            color={theme.onPrimary}
+            name="menu"
+            onPress={() => props.onMenuPress()}
+          ></Icon>
         </View>
       </View>
       <View style={{ flex: 5 }}>
-        <View style={{ alignItems: "center" }}>
+        <View style={{ alignItems: 'center' }}>
           <Text style={styles.title}>{props.title}</Text>
         </View>
       </View>
       <View style={{ flex: 1 }}>
-        <View style={{ alignItems: "center" }}>
-          <Icon name="more"></Icon>
-          
+        <View style={{ alignItems: 'center' }}>
+          <Icon color={theme.onPrimary} name="more"></Icon>
         </View>
       </View>
     </View>
   );
 }
-
-let styles = StyleSheet.create({
-  container: {
-    padding: 10,
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    marginTop: getStatusBarHeight(),
-    backgroundColor: "#fff",
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-  },
-  title: {
-    fontSize: 21,
-    fontWeight: "bold",
-    color: "#000",
-  },
-});
